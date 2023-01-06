007c125f886e0d53ddda4c32e4ca7ec4e3885378
DzhanaevArtur <janaevak@yandex.ru>
2023-01-06 23:20:52 +0300

    I'm stucked with this git

---
+++ src/main/java/ru/Dzhanaev/SpringFWStarter/lessons/mvc/lesson17/Continue.java
@@ -0,0 +1,17 @@
+package ru.Dzhanaev.SpringFWStarter.lessons.mvc.lesson17;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Artur Dzhanaev
+ * @created 06.01.2023
+ */
+@Slf4j
+public class Continue {
+
+
+    public static void main(String[] args) {
+
+        log.info("Adventures of Git");
+    }
+}
