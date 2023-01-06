c3152e08ec1a2edeee50e3ef58ee162acbb4cb28
DzhanaevArtur <janaevak@yandex.ru>
2023-01-06 22:15:54 +0300

    First commit inside Richmond branch

---
+++ src/main/java/ru/Dzhanaev/SpringFWStarter/lessons/mvc/lesson17/NewBranchTest.java
@@ -0,0 +1,17 @@
+package ru.Dzhanaev.SpringFWStarter.lessons.mvc.lesson17;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Artur Dzhanaev
+ * @created 06.01.2023
+ */
+@Slf4j
+public class NewBranchTest {
+
+
+    public static void main(String[] args) {
+
+        log.info("Oh, my f***ing GOD! Isn't it enough time to be f***ed by f***ing GIT?");
+    }
+}
